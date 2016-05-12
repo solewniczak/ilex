d6aeccd45306cee88f14380905c2bc9cac322d8b
ghi <dokuwiki@imz.re>
2016-05-12 21:15:10 +0200

    server

---
+++ js/server.js
@@ -0,0 +1,50 @@
+"use strict";
+
+var ilexServer = {};
+ilexServer.host = "ws://127.0.0.1:9000/echobot";
+ilexServer.socket = null;
+ilexServer.init = function (send, recieve) {
+	try {
+		ilexServer.socket = new WebSocket(ilexServer.host);
+		console.log('WebSocket - status '+ilexServer.socket);
+		ilexServer.socket.onopen = function(msg) {
+							   console.log("Welcome - status ", this.readyState);
+                 send();
+						   };
+		ilexServer.socket.onmessage = function(msg) {
+							   console.log("Received: ", msg.data);
+                 recieve(msg.data);
+						   };
+		ilexServer.socket.onclose = function(msg) {
+							   console.log("Disconnected - status ", this.readyState);
+						   };
+	} catch(ex) {
+		console.log(ex);
+	}
+};
+
+ilexServer.recieve = function (data) {
+
+};
+
+ilexServer.send = function (json) {
+	try {
+		ilexServer.socket.send(JSON.stringify(json));
+		console.log('Sent: ', json);
+	} catch(ex) {
+		console.log(ex);
+	}
+};
+
+ilexServer.quit = function () {
+  if (ilexServer.socket != null) {
+    console.log("Goodbye!");
+		ilexServer.socket.close();
+		ilexServer.socket = null;
+	}
+};
+
+ilexServer.reconnect = function () {
+	ilexServer.quit();
+	ilexServer.init();
+};
---
+++ server/server.php
@@ -0,0 +1,43 @@
+#!/usr/bin/env php
+<?php
+
+require_once('PHP-Websockets/websockets.php');
+
+class echoServer extends WebSocketServer {
+  //protected $maxBufferSize = 1048576; //1MB... overkill for an echo server,
+  //but potentially plausible for other applications.
+
+  protected function process ($user, $message) {
+    $json = json_decode($message);
+    $text = 'texts/'.$json->{'text'}.'.txt';
+    if (file_exists($text)) {
+      $contet = file_get_contents($text);
+      $value = json_encode(array('target' => $json->{'target'},
+                                  'text' => $contet));
+    } else {
+      $value = "error: $text not found";
+    }
+    $this->send($user,$value);
+  }
+
+  protected function connected ($user) {
+    // Do nothing: This is just an echo server, there's no need to track the user.
+    // However, if we did care about the users, we would probably have a cookie to
+    // parse at this step, would be looking them up in permanent storage, etc.
+  }
+
+  protected function closed ($user) {
+    // Do nothing: This is where cleanup would go, in case the user had any sort of
+    // open files or other objects associated with them.  This runs after the socket
+    // has been closed, so there is no need to clean up the socket itself here.
+  }
+}
+
+$echo = new echoServer("0.0.0.0","9000");
+
+try {
+  $echo->run();
+}
+catch (Exception $e) {
+  $echo->stdout($e->getMessage());
+}
