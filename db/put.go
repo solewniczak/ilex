19a42c837cad623732e7e8b0958d2d5725cf5477
sg <stgrzeszczak@gmail.com>
2016-06-19 14:41:08 +0200

    fixed rune length slices

--- db/put.go
+++ db/put.go
@@ -6,6 +6,7 @@ import (
 	"gopkg.in/mgo.v2/bson"
 	"log"
 	"time"
+	"unicode/utf8"
 )
 
 func min(a, b int) int {
@@ -16,11 +17,21 @@ func min(a, b int) int {
 	}
 }
 
+func length_of_first_n_runes_in_array(array []byte, n int) int {
+	length := 0
+	for i := 0; i < n; i++ {
+		_, l := utf8.DecodeRune(array[length:])
+		length += l
+	}
+	return length
+}
+
 func add_doc(file_contents []byte, document_name *string, database *mgo.Database) {
-	fmt.Println("Document inserted successfully!")
-	full_text := string(file_contents)
-	text_length := len(full_text)
-	runes_written, total_to_write := 0, len(full_text)
+	if !utf8.Valid(file_contents) {
+		log.Fatal("The file is not a valid utf-8 text")
+	}
+	text_length_runes := utf8.RuneCount(file_contents)
+	runes_written, bytes_written, total_runes_to_write := 0, 0, text_length_runes
 
 	slices := database.C("permascroll")
 
@@ -34,40 +45,47 @@ func add_doc(file_contents []byte, document_name *string, database *mgo.Database
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Document inserted successfully!")
 
-	slice_text_length := len(slice.Text)
-	document_address := (total_slices-1)*SLICE_SIZE + slice_text_length
+	slice_length_runes := utf8.RuneCountInString(slice.Text)
+	document_address := (total_slices-1)*SLICE_SIZE + slice_length_runes
 
-	empty_space := SLICE_SIZE - slice_text_length
+	empty_space := SLICE_SIZE - slice_length_runes
 	if empty_space != 0 {
 		// fill the slice with some of the buffer
-		to_write := min(empty_space, total_to_write)
-		slice.Text = slice.Text + full_text[runes_written:to_write]
+		runes_to_write := min(empty_space, total_runes_to_write)
+		length_to_write := length_of_first_n_runes_in_array(file_contents[bytes_written:], runes_to_write)
+		//new_capacity := slice_length_bytes + length_to_write
+		//container := make([]byte, new_capacity, new_capacity)
+		//copy(container, slice.Text)
+		//copy(container[slice_length_bytes:], file_contents[bytes_written:])
+		slice.Text = slice.Text + string(file_contents[bytes_written:bytes_written+length_to_write])
 		if err = slices.UpdateId(slice.Id, bson.M{"$set": bson.M{"Text": slice.Text}}); err != nil {
 			log.Fatal(err)
 		}
-		total_to_write -= to_write
-		runes_written += to_write
+		total_runes_to_write -= runes_to_write
+		runes_written += runes_to_write
+		bytes_written += length_to_write
 	}
 
-	for total_to_write != 0 {
+	for total_runes_to_write != 0 {
 		slice.No += 1
 		slice.Id = bson.NewObjectId()
-		to_write := min(total_to_write, SLICE_SIZE)
-		slice.Text = full_text[runes_written : runes_written+to_write]
+		runes_to_write := min(total_runes_to_write, SLICE_SIZE)
+		length_to_write := length_of_first_n_runes_in_array(file_contents[bytes_written:], runes_to_write)
+		slice.Text = string(file_contents[bytes_written : bytes_written+length_to_write])
 		if err = slices.Insert(&slice); err != nil {
 			log.Fatal(err)
 		}
-		total_to_write -= to_write
-		runes_written += to_write
+		total_runes_to_write -= runes_to_write
+		runes_written += runes_to_write
+		bytes_written += length_to_write
 	}
 
 	now := time.Now()
 	now_as_string := now.Format(time.RFC3339)
 	document := Document{bson.NewObjectId(), *document_name, "utf-8 encoded text file", "plain text", now_as_string, now_as_string, 1}
 
-	version := Version{bson.NewObjectId(), document.Id, 1, now_as_string, now_as_string, text_length, [][2]int{[2]int{0, document_address}}}
+	version := Version{bson.NewObjectId(), document.Id, 1, now_as_string, now_as_string, text_length_runes, [][2]int{[2]int{0, document_address}}}
 
 	docs := database.C("docs")
 	if err = docs.Insert(document); err != nil {
