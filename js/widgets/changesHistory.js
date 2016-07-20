cfd794f12b85820488da95ed18b5b88e7e631222
Szymon Olewniczak <ghi@imz.re>
2016-07-20 19:08:33 +0200

    Changes history fundations.

--- js/widgets/changesHistory.js
+++ js/widgets/changesHistory.js
@@ -9,10 +9,23 @@ if (ilex.widgetsCollection.console !== undefined)
   throw 'ilex.widgetsCollection.console already defined';
 
 
-ilex.widgetsCollection.changesHistory = function (windowObject) {
+ilex.widgetsCollection.changesHistory
+  = function(versionsWindowObject, documentWindowObject) {
   var that = {},
-    width = windowObject.element.data('ilex-width'),
-    height = windowObject.element.data('ilex-height');
+    width = versionsWindowObject.element.data('ilex-width'),
+    height = versionsWindowObject.element.data('ilex-height');
 
+  that.container = $('<div class="ilex-changesHistory">')
+                      .appendTo(versionsWindowObject.element);
+  
+  //add new version to versions list
+  that.addNewVersion = function(date, name, author) {
+    let $date = $('<span class="ilex-date">').text(date),
+        $name =  $('<span class="ilex-name">').text(name),
+        $author =  $('<span class="ilex-author">').text(author),
+        $div = $('<div class="ilex-version">').appendTo(that.container);
+    $div.append($date, $name, $author);
+  }
+  
   return that;
 }
