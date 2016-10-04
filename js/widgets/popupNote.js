040b190e3abbc01a73d34463dc5a11bbdd313a7d
Szymon Olewniczak <ghi@imz.re>
2016-10-04 12:48:29 +0200

    link pop-up note

--- js/widgets/popupNote.js
+++ js/widgets/popupNote.js
@@ -12,9 +12,31 @@ ilex.widgetsCollection.popupNote = function ($parentWidget, zIndex) {
   var that = {},
     zIndex = zIndex || 3;
 
+  that.container = $('<div class="ilex-popupNote">').appendTo($parentWidget)
+              .css('position', 'absolute')
+              .css('z-index', zIndex)
+              .css('border', '1px solid #000')
+              .css('font-size', '80%')
+              .css('background', '#fff')
+              .css('padding', '5px');
+  
+  that.container.hide();
+  
   that.show = function(html) {
-    
+    that.container.html(html);
+    that.container.show();
   };
+  
+  that.hide = function () {
+    that.container.hide();
+  };
+  
+  $(window).on('mousemove', function (event) {
+    that.container.offset({
+          'left': event.pageX,
+          'top': event.pageY - that.container.height() - 20
+    });         
+  });
 
   return that;
 };
